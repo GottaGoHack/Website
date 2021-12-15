b76e084c6cb889a82cdd69cf83acd85c0b4c352b
jeannemorin <46577907+jeannemorin@users.noreply.github.com>
2021-12-15 19:38:39 +0100

    Added A newpage 'Actu' and dynamic articles pages

---
+++ src/routes/actu/index.svelte
@@ -0,0 +1,122 @@
+<script context="module">
+    export const load = async ({ fetch }) => {
+        const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+        const posts = await res.json();
+        return {
+            props: {
+                posts,
+            },
+        };
+    };
+</script>
+
+<script>
+    export let posts;
+
+    import Hackathon from '/assets/hackathon.webp';
+</script>
+
+<div id="header">
+    <div class="left">
+        <div class="inner">
+            <h1>Toute L'actu Gotta Go Hack</h1>
+            <hr />
+        </div>
+    </div>
+</div>
+
+<div id="blog">
+    <div class="postGrid">
+    {#each posts as item}
+        <div class="post">
+            <img src={Hackathon} alt="Hackathon" width="400" height="250"/>
+            <div class="title">{item.title.substring(0, 20)}</div>
+            <div class="description">{item.body.substring(0, 80)}</div>
+            <div class="link"><a sveltekit:prefetch href={`/actu/${item.id}`}>Read More</a></div>
+        </div>
+    {/each}
+    </div>
+</div>
+
+<style lang="scss">
+  @use 'sass:math';
+
+  @import 'mixins';
+  @import 'vars';
+
+  #header {
+    position: relative;
+
+    .left {
+      flex: 0.5;
+
+      justify-content: center;
+      align-items: center;
+      flex-direction: column;
+
+      margin-left: 25px;
+      margin-top: 50px;
+      margin-bottom: 25px;
+
+      .inner {
+        flex-direction: column;
+
+      }
+    }
+  }
+
+    #blog {
+
+      flex-direction: column;
+      padding: 35px;
+      background-color: white;
+
+      .postGrid {
+        display: grid;
+        grid-template-columns: repeat(3, 1fr);
+        grid-gap: 35px;
+
+        padding: 35px;
+
+        .post {
+          flex-direction: column;
+          align-items: center;
+
+          .title {
+            margin: 15px 0;
+            padding: 0 5px;
+
+            font-size: 44px;
+            font-weight: bold;
+            text-transform: uppercase;
+
+            color: #333;
+
+            box-shadow: $color-yellow 0 -28px inset;
+          }
+
+          .date {
+            font-size: 14px;
+
+            color: black;
+          }
+
+          .description {
+            width: 400px;
+
+            margin-top: 20px;
+
+            font-size: 20px;
+            text-align: justify;
+
+            color: black;
+          }
+          .link {
+            margin-top:10px;
+            font-size: 20px;
+            color: black;
+          }
+        }
+      }
+    }
+</style>
